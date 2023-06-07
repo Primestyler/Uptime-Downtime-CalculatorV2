0e3a2e424a67100b11c794bfb813f63af23774ef
YOUR NAME <YOUR_EMAIL>
2023-06-07 15:01:11 +0200

    add the files

---
+++ src/main/java/com/example/uptimedowntimecalculatorv2/UptimeDowntimeCalculatorV2Application.java
@@ -0,0 +1,13 @@
+package com.example.uptimedowntimecalculatorv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptimeDowntimeCalculatorV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UptimeDowntimeCalculatorV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uptimedowntimecalculatorv2/UptimeDowntimeCalculatorV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uptimedowntimecalculatorv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UptimeDowntimeCalculatorV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
