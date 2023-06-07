89a59df1a1b19eed08b6f7154315fb5ed1589898
YOUR NAME <YOUR_EMAIL>
2023-06-07 15:47:44 +0200

    add changes to Controller

--- src/main/java/com/example/uptimedowntimecalculatorv2/Controller.java
+++ src/main/java/com/example/uptimedowntimecalculatorv2/Controller.java
@@ -1,9 +1,13 @@
 package com.example.uptimedowntimecalculatorv2;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-    @RequestMapping
+    @RequestMapping("/api/downtime")
+    public int calcDownTime(@RequestParam double percentage){
+        
+    }
 }
